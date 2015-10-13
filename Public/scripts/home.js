c11750a828c7032dbd3b0cb77f65c3e1b692fd7f
Hangeer <591417089@qq.com>
2015-10-13 16:40:44 +0800

    day5 changed some fe

--- Public/scripts/home.js
+++ Public/scripts/home.js
@@ -1,28 +1,3 @@
-
-
-//		不能获取的那个接口在第 169 行
-/*
-	+	我通过 ajax 获取的 data 是这种 [Object,Object,...] （一个 Object 是一条信息）
-		(放在数组里的，目前获取数据长度是每次获取 1 个数组，数组里有 1 个，像这样 [Object] )
-
-	+	以后传值的时候也是这么（几条放在数组里）传哈？
-		我写法默认是这样的（每次 ajax 的时候我能获取的数量是你控制，比如说五条还是啥的，给几条我就渲染几条）
-
-	+	添加了拖动计数的变量
-
-	+	还有个小小的建议，未完成和已完成方不方便分别写在
-		index.php?m=Home&c=Index&a=finishFirstLoad 
-		index.php?m=Home&c=Index&a=unfinishFirstLoad
-		里？（不方便的话拿到 已完成数据 的接口我改也行，不难）
-
-		（因为首页加载的时候是写的 index.php?m=Home&c=Index&a=firstLoad ）
-		只渲染了 未完成 的页面， 已完成 并没有初始化
-
-	+	有什么需要我准备的跟我说嘛，我对接口之前提前准备一下，这样都快一些
-	
-	+	Thanks	
-
-*/
 (function(){
 	var $btn_unfinish = $("#home-nav-unfinish"),
 		$btn_finish = $("#home-nav-finish"),
@@ -144,14 +119,16 @@
 	$('.home-inner-unfinish').dropload({  //未完成-下拉
 	    scrollArea : window,
 	    loadDownFn : function(me){
+	    	//unfinishDrag ++;
 	        $.ajax({
 	            type: 'GET',
-	            url: "index.php?m=Home&c=Index&a=LoadUnfinishedData",
+	            url: "index.php?m=Home&c=Index&a=LoadUnfinishedData&time=unfinishDrag",
 	            dataType: 'json',
 	            success: function(data){
-	            	if(data.length !== 0) {
-	            		unfinishDrag ++;
-	            	}
+	            	// if(data.length !== 0) {
+	            		unfinishDrag++;
+	            		//console.log(unfinishDrag);
+	            	// }
 	            	var result = '';
 	            	for(var i=0; i<data.length; i++){
 	            		result += '<li class="home' + 
@@ -189,16 +166,18 @@
 	$('.home-inner-finish').dropload({ //已完成-下拉
 	    scrollArea : window,
 	    loadDownFn : function(me){
+	    	// finishDrag++;
+	    	// console.log(finishDrag);
 	        $.ajax({
 	            type: 'GET',
-	            url: "index.php?m=Home&c=Index&a=LoadFinishedData",		//这里获取不到数据？我换成 LoadunFinishedData 可以获取，应该是接口有问题
+	            url: "index.php?m=Home&c=Index&a=LoadFinishedData&time=finishDrag",		//这里获取不到数据？我换成 LoadunFinishedData 可以获取，应该是接口有问题
 	            dataType: 'json',
 	            success: function(data){
 	            	console.log(data);
-	            	if(data.length !== 0) {
-	            		finishDrag++;
-	            		console.log(finishDrag);
-	            	}
+	            	// if(data.length !== 0) {
+	            	 	finishDrag++;
+	            	 	console.log(finishDrag);
+	            	// }
 	            	var result = '';
 	            	for(var i=0; i<data.length; i++){
 	            		result += '<li class="home' + 
